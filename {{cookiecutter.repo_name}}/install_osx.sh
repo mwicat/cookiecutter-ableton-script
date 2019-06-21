1ae3af753476b41b12d4ee3629bdbd219f8b188f
mwicat <mwicat@gmail.com>
2019-06-21 17:56:12 +0200

    Update install_osx.sh script

--- {{cookiecutter.repo_name}}/install_osx.sh
+++ {{cookiecutter.repo_name}}/install_osx.sh
@@ -4,6 +4,11 @@ set -o nounset
 set -o errexit
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_NAME="{{cookiecutter.package_name}}"
 
-cd '/Applications/Ableton Live 10 Suite.app/Contents/App-Resources/MIDI Remote Scripts'
-sudo ln -sfn "${SCRIPT_DIR}/{{cookiecutter.package_name}}"
+LIVE_SCRIPTS_DIR="/Applications/Ableton Live 10 Suite.app/Contents/App-Resources/MIDI Remote Scripts"
+
+cd "${LIVE_SCRIPTS_DIR}"
+sudo ln -sfn "${SCRIPT_DIR}/${SCRIPT_NAME}"
+
+echo "Installed into '${LIVE_SCRIPTS_DIR}/${SCRIPT_NAME}'"
